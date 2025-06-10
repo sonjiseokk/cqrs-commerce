8bb30c2092d46ace0be76b7e9273358cf84407fe
sonjiseokk <thswltjr11@gmail.com>
2025-06-10 19:31:22 +0900

    chore: MapStruct 라이브러리 및 공통 response 추가

---
+++ src/main/java/com/example/ecommerce/common/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .data(data)
+                .message(message)
+                .build();
+    }
+}
